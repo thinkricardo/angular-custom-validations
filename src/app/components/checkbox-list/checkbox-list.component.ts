a10926942fc94119c158688f82e91756ce4bb609
Ricardo Pereira <ricardo.mlpp@gmail.com>
2020-05-22 17:08:45 +0100

    feat: add checkbox list component

---
+++ src/app/components/checkbox-list/checkbox-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-checkbox-list',
+  templateUrl: './checkbox-list.component.html',
+  styleUrls: ['./checkbox-list.component.scss'],
+})
+export class CheckboxListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
